7d88e92d29716dcf9efa794cef9ecd1ac28c31ee
Gothax <scott990328@gmail.com>
2025-09-09 01:20:56 +0900

    feat: init project

---
+++ src/main/java/com/sbungle/sbunglebe/SbungleBeApplication.java
@@ -0,0 +1,13 @@
+package com.sbungle.sbunglebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbungleBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbungleBeApplication.class, args);
+    }
+
+}
