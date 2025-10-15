7bc0191b9dbf44b1d44a50aa52df0e9933460154
Gothax <scott990328@gmail.com>
2025-10-15 19:02:21 +0900

    docs: swagger 주석 추가

--- src/main/java/com/sbungle/sbunglebe/user/dto/request/OnboardSecondRequest.java
+++ src/main/java/com/sbungle/sbunglebe/user/dto/request/OnboardSecondRequest.java
@@ -1,9 +1,21 @@
 package com.sbungle.sbunglebe.user.dto.request;
 
 import com.sbungle.sbunglebe.user.domain.enums.PreferredGenre;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record OnboardSecondRequest(
+        @Schema(
+                description = """
+                        선호하는 장르
+                        - NOVEL: 소설
+                        - POETRY_ESSAY: 시/에세이
+                        - SELF_DEVELOPMENT: 자기계발
+                        - HUMANITIES_SOCIAL: 인문/사회
+                        - ART: 예술
+                        - ECONOMY_MANAGEMENT: 경제/경영
+                        """
+        )
         @NotNull
         PreferredGenre preferredGenre
 ) {
--- src/main/java/com/sbungle/sbunglebe/user/dto/request/OnboardThirdRequest.java
+++ src/main/java/com/sbungle/sbunglebe/user/dto/request/OnboardThirdRequest.java
@@ -1,9 +1,23 @@
 package com.sbungle.sbunglebe.user.dto.request;
 
 import com.sbungle.sbunglebe.user.domain.enums.PreferredType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record OnboardThirdRequest(
+        @Schema(
+                description = """
+                        선호하는 유형
+                        - CALM_WARM: 잔잔하고 따뜻한
+                        - THRILLING: 긴장감 넘치는
+                        - PHILOSOPHICAL_REFLECTIVE: 철학적이고 사유적인
+                        - SENSUAL_DREAMY: 감성적이고 몽환적인
+                        - CHEERFUL_LIGHT: 유쾌하고 가벼운
+                        - DEPRESSED_HEAVY: 우울하고 먹먹한
+                        - REALISTIC_SOCIAL: 현실적이고 사회적인
+                        - WITTY_CREATIVE: 기발하고 창의적인
+                        """
+        )
         @NotNull
         PreferredType preferredType
 ) {
