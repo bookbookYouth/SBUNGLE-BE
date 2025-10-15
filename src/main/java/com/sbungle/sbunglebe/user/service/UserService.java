f0533947cb98971db8880d49ff66bd12bf154563
Gothax <scott990328@gmail.com>
2025-10-15 18:56:54 +0900

    docs: 주석 추가

--- src/main/java/com/sbungle/sbunglebe/user/service/UserService.java
+++ src/main/java/com/sbungle/sbunglebe/user/service/UserService.java
@@ -17,6 +17,11 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    /**
+     * 유저 관련 공통 사용 로직 - 유저 조회. 없으면 예외 발생
+     * @param userId 유저 String ID
+     * @return UserEntity 조회된 유저 엔티티
+     */
     public UserEntity findUserByIdOrThrow(String userId) {
         return userRepository.findByUserId(userId)
                              .orElseThrow(() -> new AuthenticationException(AuthenticationErrorCode.USER_NOT_FOUND));
